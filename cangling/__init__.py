415ad5b1d9c911053b525abdd2ad54185240e765
zhangjianshe <zhangjianshe@gmail.com>
2026-01-29 16:55:25 +0800

    优化package manage

---
+++ cangling/__init__.py
@@ -0,0 +1,1 @@
+from .workflow import Workflow,ProgressContent,MessageType,ProgressMessage
--- cangling/workflow/__init__.py
+++ cangling/workflow/__init__.py
@@ -1,1 +1,1 @@
-from .workflow import  Workflow
+from .workflow import  Workflow,ProgressContent,MessageType,ProgressMessage
